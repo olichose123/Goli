2d278ec9973bf69fddcdc2533ab4be0f92ed3a87
Olivier Banville <olivier@banville.net>
2026-02-13 00:41:32 -0500

    rework geometry logic

--- Goli/Utils/Geometry.cs
+++ Goli/Utils/Geometry.cs
@@ -7,36 +7,38 @@ public class Geometry
 {
     public static Rect2 GetTargetScreenBounds(VisualInstance3D target, Camera3D camera)
     {
-        // get aabb and set global position
-        Aabb bounds = target.GetAabb();
-        bounds.Position += target.GlobalPosition + bounds.Size / 2;
+        Aabb localAabb = target.GetAabb();
+        Transform3D globalTransform = target.GlobalTransform;
 
-        // build a list of planes representing a box based on aabb
-        Array<Plane> planes = Geometry3D.BuildBoxPlanes(bounds.Size);
+        Vector3[] corners = new Vector3[8];
+        Vector3 size = localAabb.Size;
+        Vector3 pos = localAabb.Position;
 
-        // fetch each corner of the box
-        var corners = new Array<Vector3>();
-        foreach (var plane in planes)
+        corners[0] = globalTransform * pos;
+        corners[1] = globalTransform * (pos + new Vector3(size.X, 0, 0));
+        corners[2] = globalTransform * (pos + new Vector3(0, size.Y, 0));
+        corners[3] = globalTransform * (pos + new Vector3(0, 0, size.Z));
+        corners[4] = globalTransform * (pos + new Vector3(size.X, size.Y, 0));
+        corners[5] = globalTransform * (pos + new Vector3(size.X, 0, size.Z));
+        corners[6] = globalTransform * (pos + new Vector3(0, size.Y, size.Z));
+        corners[7] = globalTransform * (pos + size);
+
+        Vector2 firstScreenPos = camera.UnprojectPosition(corners[0]);
+        Vector2 min = firstScreenPos;
+        Vector2 max = firstScreenPos;
+
+        for (int i = 1; i < 8; i++)
         {
-            var corner = bounds.Position + plane.Normal * bounds.Size;
-            corners.Add(corner);
+            if (camera.IsPositionBehind(corners[i])) continue;
+
+            Vector2 screenPos = camera.UnprojectPosition(corners[i]);
+
+            min.X = Mathf.Min(min.X, screenPos.X);
+            min.Y = Mathf.Min(min.Y, screenPos.Y);
+            max.X = Mathf.Max(max.X, screenPos.X);
+            max.Y = Mathf.Max(max.Y, screenPos.Y);
         }
 
-        // determine top-left-most and bottom-right-most screen positions
-        Vector2 minScreen = Vector2.Zero;
-        Vector2 maxScreen = Vector2.Zero;
-
-        foreach (var corner in corners)
-        {
-            var screenPos = camera.UnprojectPosition(corner);
-            if (minScreen == Vector2.Zero)
-                minScreen = screenPos;
-            if (maxScreen == Vector2.Zero)
-                maxScreen = screenPos;
-            minScreen = new Vector2(Mathf.Min(minScreen.X, screenPos.X), Mathf.Min(minScreen.Y, screenPos.Y));
-            maxScreen = new Vector2(Mathf.Max(maxScreen.X, screenPos.X), Mathf.Max(maxScreen.Y, screenPos.Y));
-        }
-
-        return new Rect2(minScreen, maxScreen - minScreen);
+        return new Rect2(min, max - min);
     }
 }
